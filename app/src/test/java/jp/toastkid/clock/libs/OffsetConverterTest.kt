da9bd8bb894128de8e62f5d46998326a0f8653bd
Toast Kid <super_q1m@yahoo.co.jp>
2018-10-22 21:59:14 +0900

    Divide OffsetConverterTest's test cases to individual methods.

--- app/src/test/java/jp/toastkid/clock/libs/OffsetConverterTest.kt
+++ app/src/test/java/jp/toastkid/clock/libs/OffsetConverterTest.kt
@@ -5,18 +5,38 @@ import org.junit.Test
 import java.util.*
 
 /**
+ * [OffsetConverter]'s test cases.
+ *
  * @author toastkidjp
  */
 class OffsetConverterTest {
 
+    /**
+     * Check normal offset case.
+     */
     @Test
-    fun testInvoke() {
-        val pyongyang = TimeZone.getTimeZone("Asia/Pyongyang").rawOffset.toLong()
-        assertEquals("+08:30", OffsetConverter(pyongyang))
+    fun checkNormalCase() {
         val tokyo = TimeZone.getTimeZone("Asia/Tokyo").rawOffset.toLong()
         assertEquals("+09:00", OffsetConverter(tokyo))
+    }
+
+    /**
+     * Check minus offset case.
+     */
+    @Test
+    fun checkMinusCase() {
         val caracas = TimeZone.getTimeZone("America/Caracas").rawOffset.toLong()
         assertEquals("-04:00", OffsetConverter(caracas))
+    }
+
+    /**
+     * Check containing fraction cases.
+     */
+    @Test
+    fun checkFractionCase() {
+        val pyongyang = TimeZone.getTimeZone("Asia/Pyongyang").rawOffset.toLong()
+        assertEquals("+08:30", OffsetConverter(pyongyang))
+
         val eucla = TimeZone.getTimeZone("Australia/Eucla").rawOffset.toLong()
         assertEquals("+08:45", OffsetConverter(eucla))
     }
