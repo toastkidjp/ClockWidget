91ccc8c3b2660237b295ee26c15b6b5a6bd043d9
Toast Kid <super_q1m@yahoo.co.jp>
2018-10-23 23:09:23 +0900

    Import AdMob.

--- app/src/main/java/jp/toastkid/clock/ClockSettingsActivity.kt
+++ app/src/main/java/jp/toastkid/clock/ClockSettingsActivity.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_setting_top.*
 
 class ClockSettingsActivity : AppCompatActivity() {
@@ -13,6 +17,8 @@ class ClockSettingsActivity : AppCompatActivity() {
      */
     private var pagerAdapter: SettingPagerAdapter? = null
 
+    private lateinit var adView: AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +26,20 @@ class ClockSettingsActivity : AppCompatActivity() {
 
         pagerAdapter = SettingPagerAdapter(supportFragmentManager)
         container?.adapter = pagerAdapter
+        MobileAds.initialize(this, "ca-app-pub-5751262573448755~6177129103")
+
+        adView = AdView(this)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = "ca-app-pub-5751262573448755/7817513836"
+        ad_container.addView(adView)
+
+        val adRequest = AdRequest.Builder().addTestDevice("B4F1033D07067316E4ED247D9F18E7D7").build()
+        adView.loadAd(adRequest)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        adView.destroy()
     }
 
     companion object {
