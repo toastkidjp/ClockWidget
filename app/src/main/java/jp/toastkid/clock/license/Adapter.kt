c5f7f32a7f116da1433c72e10b8ecf46fb1217e8
Toast Kid <super_q1m@yahoo.co.jp>
2018-10-29 22:34:16 +0900

    Modify item layout. Fix #11.

--- app/src/main/java/jp/toastkid/clock/license/Adapter.kt
+++ app/src/main/java/jp/toastkid/clock/license/Adapter.kt
@@ -11,6 +11,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import jp.toastkid.clock.R
 
 /**
  * @author toastkidjp
@@ -23,14 +24,14 @@ class Adapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             ViewHolder(
                     LayoutInflater.from(parent.context)
-                            .inflate(android.R.layout.simple_list_item_1, parent, false)
+                            .inflate(R.layout.item_license_information, parent, false)
             )
 
     override fun getItemCount() = licenseAssets.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = licenseAssets[position]
-        holder.itemView.findViewById<TextView>(android.R.id.text1).text = ExtensionRemover(item)
+        holder.itemView.findViewById<TextView>(R.id.title_license).text = ExtensionRemover(item)
         holder.itemView.setOnClickListener { onClick(item) }
     }
 }
