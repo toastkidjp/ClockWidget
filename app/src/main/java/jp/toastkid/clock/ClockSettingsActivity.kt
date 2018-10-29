8d0416c52b4e605c82a310384e7c5a7e60a4208b
Toast Kid <super_q1m@yahoo.co.jp>
2018-10-29 21:05:41 +0900

    Implement basic menus.

--- app/src/main/java/jp/toastkid/clock/ClockSettingsActivity.kt
+++ app/src/main/java/jp/toastkid/clock/ClockSettingsActivity.kt
@@ -3,7 +3,10 @@ package jp.toastkid.clock
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_setting_top.*
 
 class ClockSettingsActivity : AppCompatActivity() {
@@ -21,6 +24,36 @@ class ClockSettingsActivity : AppCompatActivity() {
         pagerAdapter = SettingPagerAdapter(supportFragmentManager)
         container?.adapter = pagerAdapter
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+            when (item.itemId) {
+                R.id.menu_color -> {
+                    container?.currentItem = 1
+                    true
+                }
+                R.id.menu_time_zone -> {
+                    container?.currentItem = 0
+                    true
+                }
+                R.id.exit -> {
+                    moveTaskToBack(true)
+                    true
+                }
+                else -> {
+                    Snackbar.make(
+                            findViewById(android.R.id.content),
+                            "Implementing...",
+                            Snackbar.LENGTH_SHORT
+                    ).show()
+                    super.onOptionsItemSelected(item)
+                }
+            }
 
     companion object {
 
