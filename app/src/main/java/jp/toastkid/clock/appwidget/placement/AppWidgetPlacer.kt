2f8f994aa07eb81ff3ddf9525a5ae60daec5c314
Toast Kid <super_q1m@yahoo.co.jp>
2019-07-05 21:41:10 +0900

    refactor #26 Add comments in AppWidgetPlacer.

--- app/src/main/java/jp/toastkid/clock/appwidget/placement/AppWidgetPlacer.kt
+++ app/src/main/java/jp/toastkid/clock/appwidget/placement/AppWidgetPlacer.kt
@@ -9,14 +9,26 @@ import android.os.Bundle
 import jp.toastkid.clock.appwidget.SingleWidgetProvider
 
 /**
+ * App widget placement utility.
+ *
+ * @param context [Context]
  * @author toastkidjp
  */
 class AppWidgetPlacer(private val context: Context) {
 
+    /**
+     * App widget manager.
+     */
     private val appWidgetManager = AppWidgetManager.getInstance(context)
 
+    /**
+     * App widget's provider name.
+     */
     private val targetAppWidgetProviderName = SingleWidgetProvider::class.java.canonicalName
 
+    /**
+     * Place app widget when it's called on Android O and over.
+     */
     @TargetApi(Build.VERSION_CODES.O)
     operator fun invoke() {
         appWidgetManager.requestPinAppWidget(
@@ -26,6 +38,9 @@ class AppWidgetPlacer(private val context: Context) {
         )
     }
 
+    /**
+     * Use for checking running device is target OS.
+     */
     fun isTargetOs() =
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
                 && appWidgetManager.isRequestPinAppWidgetSupported
