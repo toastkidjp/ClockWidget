8a6296b9616ddfa85f3fa94ab1f0cc6496be7ce1
Toast Kid <super_q1m@yahoo.co.jp>
2019-07-05 21:26:07 +0900

    refactor #26 Clean up code in ColorSettingFragment.

--- app/src/main/java/jp/toastkid/clock/setting/color/ColorSettingFragment.kt
+++ app/src/main/java/jp/toastkid/clock/setting/color/ColorSettingFragment.kt
@@ -27,7 +27,11 @@ class ColorSettingFragment : Fragment(), TitleProvider {
 
     private var preferenceApplier: PreferenceApplier? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_color, container, false)
     }
